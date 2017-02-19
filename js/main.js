fc7e819dd38927d170fc08cd012fa076ba474f55
Anton Bovin <0x00019913@gmail.com>
2017-02-19 01:40:10 -0600

    added intro box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 var stage = new Stage();
+
+var introVisible = false;
+var intro = document.getElementById('intro');
+var titleChevron = document.getElementById('titleChevron');
+document.getElementById('titlebox').onclick = function(){
+  introVisible = !introVisible;
+  if (introVisible) {
+    intro.className = 'active';
+    titleChevron.className = 'up';
+  }
+  else {
+    intro.className = 'inactive';
+    titleChevron.className = 'down';
+  }
+}
