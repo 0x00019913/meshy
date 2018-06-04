a1abbe79462c5f5026776e5713ddd053fa24ce1e
Anton Bovin <0x00019913@gmail.com>
2018-06-04 11:39:13 -0500

    added

---
+++ js/mcg/generate.js
@@ -0,0 +1,16 @@
+Object.assign(MCG.Generate, (function() {
+
+  return {
+
+    infillLinear: function(min, max, context, spacing, angle, parity) {
+      context = context || new MCG.Context();
+      spacing = spacing || context.p;
+      angle = angle || 0;
+      parity = parity || 0;
+
+      
+    },
+
+  };
+
+})());
