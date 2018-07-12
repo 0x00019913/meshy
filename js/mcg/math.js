f5d99d9f28c61ec7ef2eac5b27ddabff5d5fdfea
Anton Bovin <0x00019913@gmail.com>
2018-07-12 15:48:19 -0500

    end/start intersections now considered collinear

--- js/mcg/math.js
+++ js/mcg/math.js
@@ -189,6 +189,8 @@ Object.assign(MCG.Math, (function() {
       // are collinear
       if (result & flags.a0 && result & flags.a1) return flags.collinear;
       if (result & flags.b0 && result & flags.b1) return flags.collinear;
+      if (result & flags.a0 && result & flags.b1) return flags.collinear;
+      if (result & flags.a1 && result & flags.b0) return flags.collinear;
 
       // possible intersection on intermediate points
       if (result === flags.none) {
